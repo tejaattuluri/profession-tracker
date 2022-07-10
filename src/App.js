93053ef75e5692ae46a47e7f29bcfcff5d16b266
teja <sairht304@gmail.com>
2022-07-10 17:41:40 +0530

    updated files..

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
-import './App.css';
-import React, { Component } from 'react';
+import "./App.css";
+import React, { Component } from "react";
 import Form from './components/Form';
-import Contact from './components/Contact'
+import Contact from "./components/Contact";
+import Final from "./components/Final";
 
 class App extends Component {
-  render() {    
+  render() {
     return (
       <div>
-        <div className="App-header">Login form</div>
-        <div class="App-container">
+        <div className="App-header">Contact form</div>
+         <div class="App-container">
             <div className="App-container-left"></div>
             <div className="App-container-right">
-             {/*<Form/>*/}
+             <Form/>
             </div>
-            <Contact/>
+            <div className="App-container-Final">{/*<Final/>*/}</div>
+        </div> 
+
+        <div class="Contact-container">
+          <div style={{border: "0px solid black",margin: "auto",width: "30%",bottom:"25%",position:"absolute",left:"35%"}}>{/*<Contact/>*/}</div>
         </div>
-        <div class="footer">Developed by Teja Attuluri</div>
-       {/*<div class="teja">
+
+        <div class="footer">Developed by Teja Attuluri. All copy rights reserved.</div>
+        {/*<div class="teja">
           <p>Start Date:27 June 2022.</p>
           <p> Days Elapsed:8.</p>
           <p>Due Date:15 July 2022.</p>
-    </div> */}
+        </div> */}
       </div>
-      );
+    );
   }
 }
 
---
+++ src/components/Final.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import './Final.css';
+
+
+class Final extends Component {
+    render() {
+        return ( 
+            <div>
+
+
+
+<table>
+  <tr>
+    <th>First Name</th>
+    <th>Last Name</th>
+    <th>Job Title</th>
+    <th>Location</th>
+  </tr>
+  <tr>
+    <td>Hari Krishna</td>
+    <td>Thota</td>
+    <td>Software Engineer</td>
+    <td>India</td>
+  </tr>
+  <tr>
+    <td>Kalyan</td>
+    <td>Bodapati</td>
+    <td>Mechanical Engineer</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Kalyan</td>
+    <td>Kommerla</td>
+    <td>Civil Engineer</td>
+    <td>Dubai</td>
+  </tr>
+  <tr>
+    <td>Eknath</td>
+    <td>Bhavanam</td>
+    <td>Software Engineer</td>
+    <td>USA</td>
+  </tr>
+  <tr>
+    <td>Venkat</td>
+    <td>Mainani</td>
+    <td>Cyber Security Engineer</td>
+    <td>India</td>
+  </tr>
+  <tr>
+  <td>Hari Krishna</td>
+    <td>Thota</td>
+    <td>Software Engineer</td>
+    <td>India</td>
+  </tr>
+  <tr>
+  <td>Kalyan</td>
+    <td>Bodapati</td>
+    <td>Mechanical Engineer</td>
+    <td>Germany</td>
+
+  </tr>
+  <tr>
+  <td>Kalyan</td>
+    <td>Kommerla</td>
+    <td>Civil Engineer</td>
+    <td>Dubai</td>
+
+  </tr>
+  <tr>
+  <td>Eknath</td>
+    <td>Bhavanam</td>
+    <td>Software Engineer</td>
+    <td>USA</td>
+  </tr>
+  <tr>
+  <td>Venkat</td>
+    <td>Mainani</td>
+    <td>Cyber Security Engineer</td>
+    <td>India</td>
+  </tr>
+</table>
+            </div>
+            );
+        }
+    }
+
+
+    export default Final;
