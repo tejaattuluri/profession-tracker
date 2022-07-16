85ce5e2da3cda5995589515c775bb15f226a9fd2
snigdhesh <snigdhesh357@gmail.com>
2022-07-16 11:35:55 -0400

    Corrected navbar.js and navbar.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,24 @@ import { Link } from "react-router-dom";
 import "./navbar.css";
 
 const Navbar = () => {
-    return (
-        <div class="navbar">
-            <span class="brand">Profession tracker</span>
-            <ul>
-                <Link to="/"><li>Login</li></Link>
-                <Link to="/contact"><li>Add contact</li></Link>
-                <Link to="/final"><li>Contacts</li></Link>
-            </ul>        
-        </div>
-        
-    ) 
-    
-}
+  return (
+    <div class="navbar">
+      <div class="navbar-left">Profession Tracker</div>
+      <div class="navbar-right">
+        <ul>
+          <Link to="/">
+            <li>Login</li>
+          </Link>
+          <Link to="/contact">
+            <li>Add</li>
+          </Link>
+          <Link to="/final">
+            <li>Contacts</li>
+          </Link>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default Navbar;
